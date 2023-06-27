791d11942924e3806666663e1559d262f57b62ab
Kulpot <kulpotwahu@gmail.com>
2023-06-27 23:05:48 +0800

    Add project files.

---
+++ ConditionalAttribute/Program.cs
@@ -0,0 +1,19 @@
+﻿//#define WE_BE_DEBUGIN // alternative in project properties-BUILD-CONDITIONAL Completion SYMBOLS- then copypaste WE_BE_DEBUGIN
+using System;
+using System.Diagnostics;
+
+//ref link: https://www.youtube.com/watch?v=6KkIYL_WoqE&list=PLRwVmtr-pp05brRDYXh-OTAIi-9kYcw3r&index=11&t=1s
+//
+
+class MainClass
+{
+    static void Main()
+    {
+        TraceDebuggingStuff("We are debugging here");
+    }
+    [Conditional("WE_BE_DEBUGIN")] // product shipping must vaporize before shipping
+    public static void TraceDebuggingStuff(string messageToTrace)
+    {
+        Console.WriteLine("Debugging: " + messageToTrace);
+    }
+}
